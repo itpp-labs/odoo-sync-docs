2bf1050b8884e7884f821b86f53b7839e35e3de3
Ivan Yelizariev <yelizariev@it-projects.info>
2019-08-15 14:54:37 +0000

    lambda function

--- doc-src/x2odoo/telegram2odoo-via-lambda/lambda_function.py
+++ doc-src/x2odoo/telegram2odoo-via-lambda/lambda_function.py
@@ -4,6 +4,7 @@ import os
 import logging
 import re
 import json
+ import urllib.parse as urlparse
 
 
 logger = logging.getLogger()
@@ -14,6 +15,9 @@ if LOG_LEVEL:
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 TELEGRAM_USER_ID = int(os.environ.get('TELEGRAM_USER_ID'))
+OPENAPI_SPECIFICATION_URL = int(os.environ.get('OPENAPI_SPECIFICATION_URL'))
+ODOO_DATABASE_NAME = int(os.environ.get('ODOO_DATABASE_NAME'))
+ODOO_USER_TOKEN = int(os.environ.get('ODOO_USER_TOKEN'))
 
 
 RESPONSE_200 = {
@@ -39,4 +43,20 @@ def lambda_handler(event, context):
         bot.sendMessage(chat_id=chat.id, text="This is a private bot, sorry. If you want similary bot, check documentation at website https://odoo-sync.sh")
          return RESPONSE_200
 
-    odoo = TODO
+     odoo_address = urlparse.urlparse(OPENAPI_SPECIFICATION_URL)
+     host = odoo_address.hostname
+     http_client = RequestsClient()
+     http_client.set_basic_auth(
+         host, ODOO_DATABASE_NAME, ODOO_USER_TOKEN 
+     )
+     odoo = SwaggerClient.from_url(
+         OPENAPI_SPECIFICATION_URL,
+         http_client=http_client,
+     )
+
+     res = odoo.note_note.addNoteNote(body={'memo': text})
+     note_id = TODO
+     note_url = ''.join(OPENAPI_SPECIFICATION_URL.split('/')[:3])
+     note_url = '%s/web?id=%s&view_type=form&model=note.note' % (note_url, note_id)
+     bot.sendMessage(chat_id=chat.id, text="Note is created: %s" % note_url, reply_to_message_id=message.message_id)
+     return RESPONSE_200
